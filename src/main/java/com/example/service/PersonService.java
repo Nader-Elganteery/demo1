0bb084d773516eac20671b124355a3c2bf29acd9
Nader Elganteery <86785874+Nader-Elganteery@users.noreply.github.com>
2025-09-20 20:54:51 +0300

    Update PersonService.java with webhook new ip

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -36,7 +36,7 @@ public class PersonService {
 		p = new Person();
 		p.setId("4");
 		p.setAge(25);
-		p.setFirstName("WebHook");
+		p.setFirstName("WebHook with new ip");
 		p.setLastName("Hello Jenkins");
 		persons.add(p);
 		
