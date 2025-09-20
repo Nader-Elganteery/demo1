ecef968e7f0ee2a82e42b14e7d7cfe5cbd1abdd0
Nader Elganteery <86785874+Nader-Elganteery@users.noreply.github.com>
2025-09-20 20:10:37 +0300

    Update PersonService.java using webhook

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -36,8 +36,8 @@ public class PersonService {
 		p = new Person();
 		p.setId("4");
 		p.setAge(25);
-		p.setFirstName("AbdulHalim");
-		p.setLastName("Mahmoud");
+		p.setFirstName("WebHook");
+		p.setLastName("Hello Jenkins");
 		persons.add(p);
 		
 	}
